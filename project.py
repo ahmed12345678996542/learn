12db1e0560af9956b2bc4b5601d54a2373d92700
mohamed-12-tarek <83798608+mohamed-12-tarek@users.noreply.github.com>
2025-07-22 18:09:07 +0300

    Update and rename project.js to project.py
    sum of two numbers

---
+++ project.py
@@ -0,0 +1,9 @@
+def sum(n1, n2):
+  return n1 + n2
+
+def main():
+  num1 = int(input(the first Number: ))
+  num2 = int(input(the second Number: ))
+  print(f"The Sum of two Numbers is {sum(num1, num2)}")
+
+main()
